e03abb3739f41d5f828426380897b1ad7fc7f00c
Jadrian Montilla <Jadrianmontilla06@gmail.com>
2024-10-21 10:56:54 -0400

    Create 06_01_person.py

---
+++ 06_dictionaries/06_01_person.py
@@ -0,0 +1,10 @@
+Person1 = {
+    "first_name":"John", 
+    "last_name":"Vegetabl", 
+    "age":"16", 
+    "city":"Yasoinaba"
+}
+print(Person1["first_name"])
+print(Person1["last_name"])
+print(Person1["age"])
+print(Person1["city"])
